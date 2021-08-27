5c16538758f8623e239ee9155cd76a500415cdf5
Jhoan-Ospina <jhoanospina200726@gmail.com>
2021-08-27 16:07:27 -0500

    Salones y Profesores

--- Marbic/menu.php
+++ Marbic/menu.php
@@ -8,21 +8,49 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="<?= ROOT ?>/index.php">Inicio</a>
+
+          <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Usuarios
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="<?= ROOT ?>/Usuarios/agregar.php">Registro</a></li>
+            <li><a class="dropdown-item" href="<?= ROOT ?>/Usuarios/ver.php">Ver Usuarios</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= ROOT ?>/Usuarios/agregar.php">Registro</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="profesores" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Profesores 
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="profesores">
+            <li><a class="dropdown-item" href="<?= ROOT ?>/Profesores/agregar.php">Registro</a></li>
+            <li><a class="dropdown-item" href="<?= ROOT ?>/Profesores/ver.php">Ver Profesores</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= ROOT ?>/Usuarios/ver.php">Ver usuarios</a>
+      
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="salones" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Salones
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="salones">
+            <li><a class="dropdown-item" href="<?= ROOT ?>/salones/agregar.php">Comentario</a></li>
+            <li><a class="dropdown-item" href="<?= ROOT ?>/salones/ver.php">Ver salones</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= ROOT ?>/Comentarios/agregar.php">Comentario</a>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="Comentarios" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Comentarios
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="Comentarios">
+            <li><a class="dropdown-item" href="<?= ROOT ?>/Comentarios/agregar.php">Comentario</a></li>
+            <li><a class="dropdown-item" href="<?= ROOT ?>/Comentarios/ver.php">Ver comentarios</a></li>
+          </ul>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= ROOT ?>/Comentarios /ver.php">Ver comentarios</a>
+
+
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        
         </li>
       </ul>
     </div>
